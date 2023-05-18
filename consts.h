74ec65d8cb2521bf0b9fb909cb78972ff35af18f
goonmandu <hwang.mingi7@gmail.com>
2023-05-17 22:44:42 -0400

    Add constexprs for BLACK and WHITE for use in bitboards

--- consts.h
+++ consts.h
@@ -1,6 +1,12 @@
 #pragma once
 
 constexpr int DIM = 15;
+
+// Ordinary omok board representation (std::vector<std::vector<int>>)
 constexpr int MAX = 1;
 constexpr int MIN = -1;
 constexpr int UNDEF = 0;
+
+// Bitboard omok board representation (std::vector<std::vector<bool>>)
+constexpr bool BLACK = false;
+constexpr bool WHITE = true;
