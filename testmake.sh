dfc2f7ba37e830aa4a1edde2909ddde2e0752170
goonmandu <hwang.mingi7@gmail.com>
2023-06-09 16:13:05 +0900

    Space out arguments

--- testmake.sh
+++ testmake.sh
@@ -1,2 +1,2 @@
-g++ -c Omok.cpp OmokMove.cpp bitboard_test.cpp -std=c++11 -Ofast
-g++ -o bitb_ofast Omok.cpp OmokMove.cpp bitboard_test.cpp -std=c++11 -Ofast
+g++ -c            Omok.cpp Engine.cpp OmokMove.cpp bitboard_test.cpp -std=c++11 -Ofast
+g++ -o bitb_ofast Omok.cpp Engine.cpp OmokMove.cpp bitboard_test.cpp -std=c++11 -Ofast
